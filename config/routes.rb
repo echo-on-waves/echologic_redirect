792a1c88b1039febc3057e10934d184264e437a9
Laszlo Papp <laszlo.papp@echologic.org>
2010-08-30 19:01:39 +0200

    Root URL will also be redirected.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
 
-  # All routing errors are rescued by a routine which redirects everything to echo.to 
+  # All routing errors are rescued by a routine which redirects everything to echo.to
+  map.root :controller => 'application', :action => 'redirect_to_new_host'
 
 end
